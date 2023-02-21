bac1c8bb24f02785a7c6ed93ff627dbdc12bf5f6
connorabernethy <cjaberne@asu.edu>
2023-02-20 17:42:46 -0700

    Inital environment setup and design.

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const switcher = document.getElementById('append');
+
+switcher.addEventListener('click', append_element);
+
+
+function append_element() {
+    console.log("BUTTON CLICKED");
+}
+
+function insert_element() {
+
+}
+
+function remove_element() {
+
+}
