3e9418011ffa506a3c7e8593e5fb10dff77fb662
connorabernethy <cjaberne@asu.edu>
2023-03-07 15:11:43 -0700

    Changing append, insert, and remove methods.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 class LinkedListItem extends HTMLDivElement {
     constructor() {
         super();
-        this.innerHTML = "BANG";
         //this.innerHTML = this.getAttribute('data') || undefined;
     }
 }
@@ -21,32 +20,66 @@ const append_button = document.getElementById('append');
 const insert_button = document.getElementById('insert');
 const remove_button = document.getElementById('remove');
 const input1 = document.getElementById('input1');
-const input3 = document.getElementById('input3')
+const input2 = document.getElementById('input2');
+const input3 = document.getElementById('input3');
 const container = document.getElementById('container');
 const list = document.getElementById("linked-list");
+const insertErrorLabel = document.getElementById('insertError');
 
-append_button.addEventListener('click', append_element);
+append_button.addEventListener('click', function() {append_element(input2)});
 insert_button.addEventListener('click', function() {insert_element(input1)});
 remove_button.addEventListener('click', function() {remove_element(input3)});
 
-function updateList() {
+function updateList(inputVal) {
     const children = list.children;
-    listData.forEach((item) => {
-        Array.from(children).forEach((child) => {
-            if (item === child.innerHTML) {
-                child.remove();
-            }
-        })
+    console.log(inputVal + "INPUT");
+
+    Array.from(children).forEach((child) => {
+        console.log(child.childNodes[0].innerHTML);
+        if (inputVal === child.childNodes[0].innerHTML) {
+            child.remove();
+            const index = listData.indexOf(inputVal);
+            const deletedItem = listData.splice(index, 1);
+        }
     })
 }
 
 
-function append_element(form) {
-    console.log("BUTTON CLICKED");
+function append_element(input) {
+    if (input.value.length <= 0){
+        insertErrorLabel.innerHTML = "Please input valid values."
+        return;
+    }
+    listData.push(input.value);
+
+    const linkedListItem = document.createElement("div");
+    linkedListItem.className = "list-item-container";
+
+    var div = document.createElement("linked-list-item");
+    var arrow = document.createElement("div");
+    arrow.innerHTML = "&rarr;";
+
+    div.style.padding = "1em";
+    div.style.color = "red";
+    div.style.outline = "1px solid black";
+    div.style.fontSize = 12;
+    div.style.textAlign = "center";
+
+    div.innerHTML = input.value;
+
+    linkedListItem.appendChild(div);
+    linkedListItem.appendChild(arrow);
+
+    list.appendChild(linkedListItem);
 }
 
-function insert_element(input) {
-    listData.push(input.value);
+function insert_element(input, inputIndex) {
+    insertErrorLabel.innerHTML = "";
+    if (input.value.length <= 0){
+        insertErrorLabel.innerHTML = "Please input valid values."
+        return;
+    }
+    listData.splice(inputIndex.value, 0, input.value);
 
     const linkedListItem = document.createElement("div");
     linkedListItem.className = "list-item-container";
@@ -71,15 +104,7 @@ function insert_element(input) {
 }
 
 function remove_element(inputVal) {
-    updateList();
-    listData.forEach((item) => {
-        if (inputVal.value === item) {
-            const index = listData.indexOf(item);
-
-            const deletedItem = listData.splice(index, 1);
-            console.log(deletedItem);
-        }
-    })
+    updateList(inputVal.value);
 
     console.log(listData);
 }
