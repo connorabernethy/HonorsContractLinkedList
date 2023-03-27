210c32d5f038b999e0c3d572c7c4959f7f954d3e
connorabernethy <cjaberne@asu.edu>
2023-03-26 18:17:54 -0700

    Updates to animations and functionality

--- app.js
+++ app.js
@@ -33,28 +33,34 @@ remove_button.addEventListener('click', function() {remove_element(input3)});
 
 function updateList(inputVal) {
     const children = list.children;
-    console.log(inputVal + "INPUT");
 
     Array.from(children).forEach((child) => {
         console.log(child.childNodes[0].innerHTML);
         if (inputVal === child.childNodes[0].innerHTML) {
-            child.remove();
-            const index = listData.indexOf(inputVal);
+            removeElementWithFade(child);
+            const index = listData.indexOf(parseInt(inputVal));
             const deletedItem = listData.splice(index, 1);
         }
     })
 }
 
+function removeElementWithFade(element) {
+    element.style.transition = "opacity 1s linear";
+    element.style.opacity = 0;
+    setTimeout(function() {
+        element.parentNode.removeChild(element);
+    }, 1000);
+}
+
 
 function append_element(input) {
-    if (input.value.length <= 0){
+    if (input.value.length <= 0 || !Number.isInteger(parseInt(input.value))){
         insertErrorLabel.innerHTML = "Please input valid values."
         return;
     }
-    listData.push(input.value);
+    listData.push(parseInt(input.value));
 
     const linkedListItem = document.createElement("div");
-    linkedListItem.className = "list-item-container";
 
     var div = document.createElement("linked-list-item");
     var arrow = document.createElement("div");
@@ -70,21 +76,26 @@ function append_element(input) {
 
     linkedListItem.appendChild(div);
     linkedListItem.appendChild(arrow);
-    //linkedListItem.classList.add('fade-in');
+    linkedListItem.classList.add('list-item-container', 'new-item');
 
     list.appendChild(linkedListItem);
 }
 
 function insert_element(input, inputIndex) {
     insertErrorLabel.innerHTML = "";
-    if (input.value.length <= 0 || inputIndex.value.length <= 0){
+    // Handle invalid inputs
+    if (input.value.length <= 0 || inputIndex.value.length <= 0 || !Number.isInteger(parseInt(input.value)) || !Number.isInteger(parseInt(inputIndex.value))){
         insertErrorLabel.innerHTML = "Please input valid values."
         return;
     }
-    listData.splice(inputIndex.value, 0, input.value);
+
+    if (inputIndex.value > listData.length && listData.length != 0 || inputIndex.value < 0) {
+        insertErrorLabel.innerHTML = "Please input a valid index."
+        return;
+    }
+    listData.splice(parseInt(inputIndex.value), 0, parseInt(input.value));
 
     const linkedListItem = document.createElement("div");
-    linkedListItem.className = "list-item-container";
 
     var div = document.createElement("linked-list-item");
     var arrow = document.createElement("div");
@@ -100,8 +111,10 @@ function insert_element(input, inputIndex) {
 
     linkedListItem.appendChild(div);
     linkedListItem.appendChild(arrow);
+    linkedListItem.classList.add('list-item-container', 'new-item');
 
-    list.appendChild(linkedListItem);
+    list.insertBefore(linkedListItem, list.children[parseInt(inputIndex.value)]);
+    //list.appendChild(linkedListItem);
     console.log(listData);
 }
 
